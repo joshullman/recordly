f9df03e38b231a950a382408a3cad34f0f6c36a8
Josh Ullman <joshullman@gmail.com>
2017-01-30 11:38:26 -0800

    add artist, album, and song models

---
+++ db/migrate/20170130193724_create_songs.rb
@@ -0,0 +1,12 @@
+class CreateSongs < ActiveRecord::Migration[5.0]
+  def change
+    create_table :songs do |t|
+      t.string   :title
+      t.integer  :album_id
+      t.integer  :track_num
+      t.integer  :length
+
+      t.timestamps
+    end
+  end
+end
