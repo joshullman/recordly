4cd9e657286b0920f647b538f72bb95eb88e805d
Josh Ullman <joshullman@gmail.com>
2017-01-30 12:05:34 -0800

    add users/show, create seed for db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.create(email: "guest@aol.com", password: "password")
+
+artist = Artist.create(name: "Sloopowerps")
+album_one = Album.create(title: "Dungalo", artist_id: artist.id)
+album_two = Album.create(title: "Meera", artist_id: artist.id)
+
+Song.create(title: "alskjdfkj", album_id: album_one.id, track_num: 1)
+Song.create(title: "fds", album_id: album_one.id, track_num: 2)
+Song.create(title: "lsjhkd", album_id: album_one.id, track_num: 3)
+Song.create(title: "mjhiuh", album_id: album_two.id, track_num: 1)
+Song.create(title: "iudhfb", album_id: album_two.id, track_num: 2)
+Song.create(title: "dgkjshdf", album_id: album_two.id, track_num: 3)
