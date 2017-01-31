00fd74e8fb7bfcff9fa5176b8f4af615b29c531d
Josh Ullman <joshullman@gmail.com>
2017-01-30 19:04:23 -0800

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,20 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :users
-  resources :artists
-  resources :albums
-  resources :songs
+  devise_scope :user do
+    authenticated :user do
+      root 'users#show', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
+
+  resources :users do
+	  resources :artists
+	  resources :albums
+	  resources :songs
+	end
+
 end
