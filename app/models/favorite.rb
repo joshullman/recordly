955c8744c0592306c6d7676bd11bbe4207d0175f
Josh Ullman <joshullman@gmail.com>
2017-01-31 13:16:09 -0800

    add find_associated_media to favorite model and fix users show

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,4 +1,16 @@
 class Favorite < ApplicationRecord
   belongs_to :favoriteable
   belongs_to :user
+
+  def find_associated_media
+  	id = self.favoriteable_id
+  	case self.favoriteable_type
+  	when "Artist"
+  		Artist.find(id)
+  	when "Album"
+  		Album.find(id)
+  	when "Song"
+  		Song.find(id)
+  	end
+  end
 end
